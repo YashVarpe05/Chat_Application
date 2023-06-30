e7dd215a2495db08627e9a1e67bb0c6addea1f59
YashVarpe05 <yashvarpe2005@gmail.com>
2023-06-30 22:11:19 +0530

    link main.js

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -1,10 +1,10 @@
 // webpack.config.js
-const path = require("path");
+// const path = require("path");
 
-module.exports = {
-	entry: "./main.js", // The entry point for bundling
-	output: {
-		path: path.resolve(__dirname, "scripts"), // Output directory for the bundled file
-		filename: "script.js", // The name of the bundled file
-	},
-};
+// module.exports = {
+// 	entry: "./main.js", // The entry point for bundling
+// 	output: {
+// 		path: path.resolve(__dirname, "scripts"), // Output directory for the bundled file
+// 		filename: "script.js", // The name of the bundled file
+// 	},
+// };
